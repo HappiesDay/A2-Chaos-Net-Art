414bef3279c2a660f78b734696691e4ce22afae5
HappiesDay <162384385+HappiesDay@users.noreply.github.com>
2024-05-08 18:02:27 +1000

    Add history shift

--- public/script.js
+++ public/script.js
@@ -34,7 +34,7 @@ class Line{
       this.history = [{x: this.x, y: this.y}]
       this.lineWidth = Math.floor(Math.random() * 15 + 1)
       this.hue = Math.floor(Math.random() * 360)
-
+      this.maxLength = 5
    }
 
    draw(context){
@@ -42,11 +42,6 @@ class Line{
       context.lineWidth = this.lineWidth
       context.beginPath()
       context.moveTo(this.history[0].x, this.history[0].y)
-      for (let i = 0; i < 3; i++){
-         this.x = Math.random() * this.cnv.width
-         this.y = Math.random() * this.cnv.height
-         this.history.push({x: this.x, y: this.y})
-      }
       for (let i = 0; i < this.history.length; i++){
          context.lineTo(this.history[i].x, this.history[i].y)
       }
@@ -54,26 +49,38 @@ class Line{
       context.lineTo(this.endX, this.endY)
       context.stroke()
    }
+   update(){
+      this.x = Math.random() * this.cnv.width
+      this.y = Math.random() * this.cnv.height
+      this.history.push({x: this.x, y: this.y})
+      if (this.history.length > this.maxLength)
+         {
+            this.history.shift()
+         }
+   }
 }
 
 const linesArray = []
-for (let i = 0; i < 10; i ++){
+const numberOfLines = 2
+for (let i = 0; i < numberOfLines; i ++){
    linesArray.push(new Line(cnv))
 
 }
-
 console.log(linesArray)
 
 //Recursion
 function animate(){
    ctx.clearRect(0, 0, cnv.width, cnv.height)
-   linesArray.forEach(line => line.draw(ctx))
-
+   //drawline
+   linesArray.forEach(line => {
+      line.draw(ctx)
+      line.update()
+   })
+   
    requestAnimationFrame(animate)
 }
-console.log('animating')
-
 animate()
 
 
+console.log('animating')
 console.log(ctx)
