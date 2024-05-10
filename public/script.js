921f91504bd04b424f5813c2258100f765db3430
HappiesDay <162384385+HappiesDay@users.noreply.github.com>
2024-05-11 00:54:37 +1000

    Add audio

--- public/script.js
+++ public/script.js
@@ -346,3 +346,154 @@ console.log(lineAnimationActive)
 //             ctx.clearRect(0, 0, cnv.width, cnv.height);
 
 // }
+
+
+
+
+
+
+
+
+
+
+
+//AUDIO 
+
+const audio_context = new AudioContext ()
+
+// getting the audio context function
+function init_audio() {
+    if (!audio_context) {
+        audio_context = new (window.AudioContext || window.webkitAudioContext)();
+    }
+}
+
+// array of notes for the sounds
+const notes = [61, 66, 69, 73, 74, 73, 69, 66]
+
+// declaring a mutable iterator
+let i = 0
+
+// declaring a mutable state value
+let running = false
+
+// declaring a mutable variable for 
+// the period of time between notes
+let period = 200
+
+// declaring a mutable variable for
+// the length of the note
+let len = 0
+function next_note () {
+
+    // use the iterator to select a note from 
+    // the notes array and pass it to the 
+    // play_note function along with the 
+    // len variable to specify the length of the note
+    play_note (notes[i], len)
+
+    // iterate the iterator
+    i++
+
+    // if i gets too big
+    // cycle back to 0
+    i %= notes.length
+}
+
+function note_player () {
+
+    // play the next note
+    next_note ()
+
+    // if running is true
+    // it uses setTimeout to call itself 
+    // after period milliseconds
+    if (running) setTimeout (note_player, period)
+}
+
+
+
+function play_note (note, length) {
+
+    // if the audio context is not running, resume it
+    if (audio_context.state != 'running') init_audio ()
+
+    // create an oscillator
+    const osc = audio_context.createOscillator ()
+
+    // make it a triangle wave this time
+    osc.type = 'sine'
+
+    // set the value using the equation 
+    // for midi note to Hz
+    osc.frequency.value = 440 * 2 ** ((note - 69) / 12)
+
+    // create an amp node
+    const amp = audio_context.createGain ()
+
+    // connect the oscillator 
+    // to the amp
+    // to the audio out
+    osc.connect (amp).connect (audio_context.destination)
+
+    // the .currentTime property of the audio context
+    // contains a time value in seconds
+    const now = audio_context.currentTime
+
+    // make a gain envelope
+    // start at 0
+    amp.gain.setValueAtTime (0, now)
+
+    // take 0.02 seconds to go to 0.4, linearly
+    amp.gain.linearRampToValueAtTime (0.4, now + 0.02)
+
+    // this method does not like going to all the way to 0
+    // so take length seconds to go to 0.0001, exponentially
+    amp.gain.exponentialRampToValueAtTime (01, now + length)
+
+    // start the oscillator now
+    osc.start (now)
+
+    // stop the oscillator 1 second from now
+    osc.stop  (now + length)
+}
+
+// declaring a function that plays the next note
+
+// this is a recursive function
+
+// this function handles the mouse event
+// when the cursor enters the canvas
+cnv.onpointerenter = e => {
+
+    // set running to true
+    running = true;
+
+    // initiate the recurseive note_player function
+    note_player ()
+}
+
+// this function handles the mouse event
+// when the cursor moves over the canvas
+cnv.onpointermove = e => {
+
+    // as the cursor goes from left to right
+    // len gos from 0 to 5
+    len = 2 * e.offsetX / cnv.width
+
+    // as the cursor goes from bottom to top
+    // period goes from 420 to 20 (milliseconds)
+    period = 200 + ((e.offsetY / cnv.height) ** 2) * 40
+}
+
+// this function handles the mouse event
+// when the cursor leaves the canvas
+cnv.onpointerleave = e => {
+
+    // set running to false
+    running = false
+}
+
+// function that handles the mouse event 
+// when cursor enters the canvas
+// it will call the sortPixels function from the sorter
